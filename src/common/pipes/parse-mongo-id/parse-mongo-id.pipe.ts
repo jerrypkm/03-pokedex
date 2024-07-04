8f5110f30cf20f883b7827c0c6decc046b1581e7
Gerardo Chapa <gerardochapa1000@gmail.com>
2024-07-03 21:58:54 -0600

    Feat: CRUD Pokedex

---
+++ src/common/pipes/parse-mongo-id/parse-mongo-id.pipe.ts
@@ -0,0 +1,17 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  PipeTransform,
+} from '@nestjs/common';
+import { isValidObjectId } from 'mongoose';
+
+@Injectable()
+export class ParseMongoIdPipe implements PipeTransform {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  transform(value: string, metadata: ArgumentMetadata) {
+    if (!isValidObjectId(value))
+      throw new BadRequestException(`${value} is not valid MongoID`);
+    return value;
+  }
+}

124db550052a1469902e68d954034ed61c84b0a3
Gerardo Chapa <gerardochapa1000@gmail.com>
2024-08-04 09:54:59 -0600

    Feat: enviroment variables configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,24 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfig } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfig],
+      validationSchema: JoiValidationSchema,
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: process.env.MONGO_DB_NAME,
+    }),
 
     PokemonModule,
 
--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
+
+  console.log('App runing in port ', process.env.PORT);
 }
 bootstrap();
